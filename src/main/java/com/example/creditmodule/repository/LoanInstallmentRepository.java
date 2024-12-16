d7cf71f09c5fec9c6d0eafd783fb3ca064ec3a85
dilemunal <diilemunal@gmail.com>
2024-12-16 22:35:20 +0300

    LoanInstallmentRepository & LoanRepository implemented

--- src/main/java/com/example/creditmodule/repository/LoanInstallmentRepository.java
+++ src/main/java/com/example/creditmodule/repository/LoanInstallmentRepository.java
@@ -3,5 +3,9 @@ package com.example.creditmodule.repository;
 import com.example.creditmodule.entity.LoanInstallment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LoanInstallmentRepository extends JpaRepository<LoanInstallment,Long> {
+    List<LoanInstallment> findByLoanId(Long loanId);
+
 }
--- src/main/java/com/example/creditmodule/repository/LoanRepository.java
+++ src/main/java/com/example/creditmodule/repository/LoanRepository.java
@@ -4,7 +4,11 @@ import com.example.creditmodule.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    List<Loan> findByCustomerId(Long customerId);
+
 
 }
