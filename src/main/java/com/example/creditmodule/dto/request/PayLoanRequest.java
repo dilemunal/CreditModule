3beeef4f6a1e494d29a5deffa559692206adfae6
dilemunal <diilemunal@gmail.com>
2024-12-17 22:48:31 +0300

    payLoan request

---
+++ src/main/java/com/example/creditmodule/dto/request/PayLoanRequest.java
@@ -0,0 +1,23 @@
+package com.example.creditmodule.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class PayLoanRequest {
+    @NotNull(message = "Loan ID cannot be null.")
+    private Long loanId;
+
+    @NotNull(message = "Payment amount cannot be null.")
+    @Positive(message = "Payment amount must be greater than 0.")
+    private Double paymentAmount;
+
+    @NotNull(message = "Payment date cannot be null.")
+    private LocalDate paymentDate;
+
+}
